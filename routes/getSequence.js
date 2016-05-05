a4d7f6889b29728fb55aaf4797db4b97f0884054
Ethan Li <lietk12@gmail.com>
2016-05-05 15:32:04 -0700

    Replaced Hello World packet with static sequence
    Sequence grabbed from TeselaGen's endpoint.

--- routes/getSequence.js
+++ routes/getSequence.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json(200, {message: "Hello, world!"});
+  res.status(200).json({
+    sequence: "tctgcctcgttccatcggaagtgctatcatgggt",
+    sequenceLength: 34,
+    features: [
+      {name: "Restriction Site 1", index: 5},
+      {name: "Promoter2", index: 26},
+      {name: "Promoter1", index: 9},
+      {name: "CDS", index: 21},
+      {name: "Promoter2", index: 5},
+      {name: "CDS2", index: 29},
+      {name: "Promoter1", index: 30},
+      {name: "Promoter1", index: 0},
+      {name: "CDS2", index: 16}
+    ]
+  });
 });
 
 module.exports = router;
